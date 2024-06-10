b4eff98115cf80770ad675152f3d8852e1012e8b
Shivek Soni <shivek6856@gmail.com>
2024-06-11 01:01:57 +0530

    added some functionalities

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import TextForm from "./Components/TextForm";
+import PropTypes from "prop-types";
+import Alert from "./Components/Alert";
+import About from "./Components/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark Mode has been enabled", "success");
+      document.title = "Textutils - Dark Mode";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode has been enabled", "success");
+      document.title = "Textutils - Light Mode";
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar
+          title="TextUtils"
+          aboutText="About TextUtils"
+          toggleMode={toggleMode}
+        />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route
+              path="/"
+              element={
+                <TextForm
+                  heading="Enter the text to analyze below"
+                  mode={mode}
+                  showAlert={showAlert}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  aboutText: PropTypes.string.isRequired,
+};
+
 export default App;
+// Navbar.defaultProps = {
+//   aboutText: "About",
+// };
---
+++ src/Components/TextForm.js
@@ -0,0 +1,123 @@
+import React, { useState, useMemo } from "react";
+
+function TextForm(props) {
+  const [text, setText] = useState("");
+
+  const handleInputChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleConvertToUpperCase = () => {
+    setText(text.toUpperCase());
+    props.showAlert("Coverted to Uppercase", "Success");
+  };
+
+  const handleConvertToLowerCase = () => {
+    setText(text.toLowerCase());
+    props.showAlert("Coverted to Lowercase", "Success");
+  };
+  const clearText = () => {
+    setText("");
+    props.showAlert("text Cleared", "Success");
+  };
+  const handleCopy = () => {
+    const textarea = document.getElementById("myBox"); // Select the textarea element
+    textarea.select(); // Select its content
+    navigator.clipboard.writeText(textarea.value);
+    props.showAlert("Copied to Clipboard", "Success"); // Copy the selected text to the clipboard
+  };
+  const handleExtraSpaces = () => {
+    let newText = text.split(/\s+/); // Split by one or more whitespace characters
+    setText(newText.join(" ")); // Join with a single space
+    props.showAlert("Extra Spaces Removed", "Success");
+  };
+
+  const wordCount = useMemo(() => {
+    return text.split(/\s+/).filter((word) => word !== "").length;
+  }, [text]);
+
+  const characterCount = useMemo(() => {
+    return text.replace(/\s/g, "").length;
+  }, [text]);
+
+  const estimatedReadingTime = useMemo(() => {
+    return 0.008 * wordCount;
+  }, [wordCount]);
+
+  return (
+    <>
+      <div
+        className="mb-3 container"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <h1>{props.heading}</h1>
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleInputChange}
+          placeholder="Enter text here"
+          id="myBox"
+          rows="8"
+          style={{
+            backgroundColor: props.mode === "dark" ? "grey" : "white",
+            color: props.mode === "dark" ? "white" : "black",
+          }}
+        />
+      </div>
+      <button
+        className="btn btn-primary mx-2"
+        onClick={handleConvertToUpperCase}
+        disabled={text.trim() === ""}
+      >
+        Convert To Uppercase
+      </button>
+      <button
+        className="btn btn-primary mx-2"
+        onClick={handleConvertToLowerCase}
+        disabled={text.trim() === ""}
+      >
+        Convert To Lowercase
+      </button>
+      <button
+        className="btn btn-primary mx-2"
+        onClick={clearText}
+        disabled={text.trim() === ""}
+      >
+        Clear Text
+      </button>
+      <button
+        className="btn btn-primary mx-2"
+        onClick={handleCopy}
+        disabled={text.trim() === ""}
+      >
+        Copy Text
+      </button>
+      <button
+        className="btn btn-primary mx-2"
+        onClick={handleExtraSpaces}
+        disabled={text.trim() === ""}
+      >
+        Remove Extra Spaces
+      </button>
+
+      <div
+        className="container my-3"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <h2>Your text summary</h2>
+        <p>
+          {wordCount} words and {characterCount} characters
+        </p>
+        <p>{estimatedReadingTime.toFixed(2)} minutes to read</p>
+        <h2>Preview</h2>
+        <p>
+          {text.length > 0
+            ? text
+            : "Enter something in the textbox to preview it here"}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default TextForm;
